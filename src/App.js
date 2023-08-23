2ee57534cdb0ad4e555727830a1b7542359a3c01
SUJIN PARK <rabbitlover1@naver.com>
2023-08-23 15:23:49 +0900

    3page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Container, Nav, Navbar } from 'react-bootstrap';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Second from './pages/second';
+import Third from './pages/third';
 
 
 function App() {
@@ -16,14 +17,16 @@ function App() {
             <Navbar.Brand href='/'>Music</Navbar.Brand>
             <Nav className='me-auto'>
               <Nav.Link href='/'>1</Nav.Link>
-              <Nav.Link href='/2'>2</Nav.Link>              
+              <Nav.Link href='/2'>2</Nav.Link>      
+              <Nav.Link href='/3'>3</Nav.Link>
             </Nav>
           </Container>
         </Navbar>
 
         <Routes>
           <Route path="/" element={<Home />} /> 
-          <Route path="/2" element={<Second />} />          
+          <Route path="/2" element={<Second />} /> 
+          <Route path="/3" element={<Third />} />         
         </Routes>
       </div>
     </Router>
---
+++ src/pages/third.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const third = () => {
+  return (
+    <div>third</div>
+  )
+}
+
+export default third
