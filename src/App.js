b398364975ead103bd0985dd31d9cc422e045afc
kangsiii <lks405jj@naver.com>
2023-08-23 15:03:11 +0900

    기본 Nav바 생성하고 Home,second 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-
+import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Second from './pages/second';
+
 
 function App() {
   return (
-    <div>
-      1
-      2
-      3
-      안녕하세요 푸시해보겠습니다!! 
-      <a>푸시푸시</a>
-    </div>
+    <Router>
+      <div className='APP'>
+        <Navbar bg='dark' variant="dark">
+          <Container className='barList'>
+            <Navbar.Brand href='/'>Music</Navbar.Brand>
+            <Nav className='me-auto'>
+              <Nav.Link href='/'>1</Nav.Link>
+              <Nav.Link href='/2'>2</Nav.Link>              
+            </Nav>
+          </Container>
+        </Navbar>
+
+        <Routes>
+          <Route path="/" element={<Home />} /> 
+          <Route path="/2" element={<Second />} />          
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
